80867ab0771a23241e3fd43ad3ab9e6d5a418484
liujiayou <liujiayou@ldrobot.com>
2022-01-18 18:18:16 +0800

    feat: fastapi 学习

--- fastapi_learn.py
+++ fastapi_learn.py
@@ -4,10 +4,121 @@
 @Author  : JiaYou
 @Time    : 2022/1/16 22:36
 """
+from typing import Optional, List
+
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path, Body, Cookie, Header
+from pydantic import BaseModel, Field, HttpUrl
 
 app = FastAPI()
+
+
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
+
+
+class Item(BaseModel):
+    name: str = Field(
+        None, title="The description of the item", max_length=5
+    )
+    price: float = Field(..., gt=0, description="The price must be greater than zero")
+    is_offer: Optional[bool] = None
+    images: Optional[List[Image]] = None
+
+    # 示例参数
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "is_offer": False,
+                "price": 1.5
+            }
+        }
+
+
+class User(BaseModel):
+    username: str
+    full_name: Optional[str] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+"""
+显式声明路径放前面，匹配路径放后面！！
+"""
+
+
+@app.get("/items/cookie")
+def read_items(ads_id: Optional[str] = Cookie(None)):
+    return {"ads_id": ads_id}
+
+
+@app.get("/items/header")
+def read_items(x_token: Optional[List[str]] = Header(None)):
+    # Header 将把参数名称的字符从下划线 (_) 转换为连字符 (-) 来提取并记录 headers.
+    return {"X-Token values": x_token}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int = Path(..., title='this is item id'),
+              q: Optional[List[str]] = Query(default=['abc', 'def', 'fff'], min_length=3, max_length=5)):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(
+        item_id: int, item: Item, user: User, importance: int = Body(...)
+):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    return results
+
+
+@app.post("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item": item, "item_id": item_id}
+
+
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: str
+    full_name: Optional[str] = None
+
+
+class UserOut(BaseModel):
+    username: str
+    email: str
+    full_name: Optional[str] = None
+
+
+class UserInDB(BaseModel):
+    username: str
+    hashed_password: str
+    email: str
+    full_name: Optional[str] = None
+
+
+def fake_password_hasher(raw_password: str):
+    return "supersecret" + raw_password
+
+
+def fake_save_user(user_in: UserIn):
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print("User saved! ..not really")
+    return user_in_db
+
+
+@app.post("/user/", response_model=UserOut)
+def create_user(user: UserIn):
+    user_saved = fake_save_user(user)
+    return user_saved
+
 
 if __name__ == '__main__':
     """fast api 使用
--- sqlalchemy_learn.py
+++ sqlalchemy_learn.py
@@ -4,10 +4,23 @@
 @Time    :   2022/1/14 16:36
 @Author  :   JiaYou
 """
-from sqlalchemy import Column, String, create_engine
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, String, create_engine, Integer, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
 Base = declarative_base()
+
+
+class Address(Base):
+    __tablename__ = 'address'
+    id = Column(Integer, primary_key=True)
+    street_name = Column(String(64))
+    post_code = Column(String(10))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    user = relationship("User", back_populates="address")
+
+    def __repr__(self):
+        return f"<Address(id={self.id}, street_name='{self.street_name}', post_code={self.post_code}, user_id={self.user_id})>"
 
 
 class User(Base):
@@ -15,15 +28,81 @@ class User(Base):
     __tablename__ = 'user'
 
     # 表的结构:
-    id = Column(String(20), primary_key=True)
-    name = Column(String(20))
+    id = Column(Integer, primary_key=True)
+    name = Column(String(16))
+    age = Column(Integer)
+
+    address = relationship("Address", back_populates="user", order_by=Address.id)
+
+    def __repr__(self):
+        return f"<User(id={self.id}, name='{self.name}', age={self.age}, address={self.address})>"
 
 
 if __name__ == '__main__':
-    engine = create_engine('mysql+pymysql://ldrobot:ldrobot2022@localhost:3306/auto_detection')
-    DBSession = sessionmaker(bind=engine)
-    session = DBSession()
-    new_user = User(id='5', name='Bob')
-    session.add(new_user)
+    # echo=True 打印生成执行SQL中日志
+    engine = create_engine('mysql+pymysql://ldrobot:ldrobot2022@localhost:3306/auto_detection', echo=True)
+    # 创建表，不存在则创建，存在则跳过
+    User.metadata.create_all(engine)
+    # 未指定的属性为None，egg: id=None
+    user = User(name='jiayou1', age=32)
+    """
+    References: https://docs.sqlalchemy.org/en/14/orm/tutorial.html#creating-a-session
+    1. 创建Engine对象
+    2. 构造工厂类Session（将Engine对象注入Session类）
+        实现方式1：
+            engine = create_engine('sqlite:///:memory:', echo=True)
+            Session = sessionmaker(bind=engine)
+        实现方式2：
+            Session = sessionmaker()
+            engine = create_engine('sqlite:///:memory:', echo=True)
+            Session.configure(bind=engine)
+    3. 实例化Session对象
+    4. 使用Session对象（Session对象从Engine中连接池获取一个连接给Session对象，直到commit或关闭session对象释放连接）
+    """
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    # 每次add都会生成一个自增ID，若没有提交该次add，依然会按序号自增
+    user.address = [Address(street_name='test street', post_code='123333'),
+                    Address(street_name='test street 2', post_code='123443')]
+    session.add(user)
+    # print(session.query(User).filter(User.name == 'zhangsan').all())
+    # print(session.query(Address).filter(Address.id.in_([1, 2])).all())
     session.commit()
-    session.close()
+
+    # for u, a in session.query(User, Address).filter(User.id == Address.user_id).filter(Address.street_name == '南山街道').all():
+    #     print(u)
+    #     print(a)
+    connection = session.connection()
+    # 使用原生SQL
+    print([dict(zip(result.keys(), result)) for result in connection.execute("select * from user").fetchall()])
+    # for item in session.query(Address).select_from(Address).join(User, User.id == Address.user_id).all():
+    #     print(item)
+
+    # u = session.get(User, 3)
+    u = session.query(User.age.label('l_age'), User.name).filter(User.id == 4).all()
+    print([dict(zip(result.keys(), result)) for result in u])
+    # u.delete()
+    # session.commit()
+    """
+    放入对象     ==>     session.new 生成
+    """
+    # print(session.new)
+    # print(session.dirty)  # IdentitySet([])
+    # our_user = session.query(User).filter_by(name='jiayou').first()
+    # print(our_user is user)  # True
+    # user.age = 99
+    """
+    user = User(name='jiayou', age=22, address_id=1)
+    1. session.add(user)
+    2. our_user = session.query(User).filter_by(name='jiayou').first()
+    3. user.age = 99
+    放入对象-->查询对象-->修改对象    ==>     session.dirty 生成
+    """
+    # print(session.dirty)  # IdentitySet([<User(name='jiayou', age=99, address_id=1)>])
+    # print(our_user.id)
+    # query = session.query(User).filter(User.name.like('%xiao%')).order_by(User.id.desc())
+    # print(query.all())
+    # for instance in session.query(User).filter(User.name.like('%xiao%')):
+    #     print(instance)
+    # session.commit()
