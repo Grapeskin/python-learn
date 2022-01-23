a050c9df979aca48ca991aef320bc6cf883fd9b0
jiayou.liu <1229738142@qq.com>
2022-01-23 12:27:23 +0800

    feat: 学习minio rabbitmq

---
+++ rabbitmq_producer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Author  : JiaYou
+@Time    : 2022/1/23 10:49
+"""
+import time
+
+import pika
+
+parameters = (
+    pika.ConnectionParameters(host='localhost', port=5672)
+)
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+for i in range(10000):
+    channel.basic_publish(exchange='', routing_key='hello',
+                          body=bytes(f'Test message [{i}].'.encode('utf-8')))
+    time.sleep(0.5)
+
+connection.close()
